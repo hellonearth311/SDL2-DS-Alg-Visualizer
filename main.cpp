518e155ec1854ef6f9b75e8495b6bd0b5668ac8e
swaritnarang <naranswarit02@gmail.com>
2026-02-02 20:40:15 -0600

    initial commit: setup window and renderer

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <SDL.h>
+#include <iostream>
+#include <stdio.h>
+
+// window dimensions
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main(int argc, char* args[])
+{
+    // stinky boilerplate
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    SDL_Event e;
+
+    bool running = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO < 0)) {
+        // ha loser ur window doesnt work
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+    SDL_SetWindowTitle(window, "DSA Visualizer");
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
+    if (window == NULL || renderer == NULL) {
+        printf("Error creating window or renderer: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // finally looping
+    while (running) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    // bye bye
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+	return 0;
+}
